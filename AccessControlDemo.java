663e6104d9ab47a10067145dd9b3414ef5955aab
Duleeka.Munasinghe <duleekalakm@gmail.com>
2023-03-14 20:34:36 +1300

    Test Access Control

---
+++ AccessControlDemo.java
@@ -0,0 +1,19 @@
+package AssistedPractice;
+//Feb 6th video 1.15 -> 
+
+public class AccessControlDemo {
+
+	int a; //default access
+	public int b; // public access
+	private int c; //private access
+
+	void setC(int i)
+	{
+		c=i;
+	}
+	int getC()
+	{
+		return c;
+	}
+
+}
