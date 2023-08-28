19edfd67b1f559787da23e2e99aaaf57767a84ac
Duleeka Munasinghe <duleekalakm@gmail.com>
2023-08-28 22:27:31 +1200

    ohhh yeah

---
+++ Inheritance/FinalDemo.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class FinalDemo {
+	
+	final int maxAge=100;
+	//final variable : value of final variable cannot
+	
+	public void setAge() {
+	//	maxAge=100; // once you assigned a value for final variable you can't changed it.
+	}
+
+}
---
+++ Inheritance/InheritanceA.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class InheritanceA {
+	
+	int i,j; //data members
+	
+	void showij() //method
+	{
+	System.out.println("value of i and j "+ i + "," + j);	
+	}
+
+}
